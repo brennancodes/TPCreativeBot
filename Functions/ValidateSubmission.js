e99f319c0065b4976634c6fc88b69c0defc8151d
BrennanDesktop <brennancodes@gmail.com>
2026-01-08 12:51:39 -0600

    I edited something I didn't need to and broke it

--- Functions/ValidateSubmission.js
+++ Functions/ValidateSubmission.js
@@ -28,27 +28,27 @@ module.exports = async (client, interaction) => {
     if (config.mtcSettings.preventResubmit || config.mtcSettings.submitCooldownHours > 0) {
         await mtcChannel.messages.fetch({limit:100}).then(async fetchedMessages =>{
             fetchedMessages.sort(function(a,b){
-                return b.message.createdTimestamp-a.message.createdTimestamp;
+                return b.createdTimestamp-a.createdTimestamp;
             })
             const msgArray = Array.from(fetchedMessages);
             for (var i = 0; i < msgArray.length; i++){
                 var x = msgArray[i][1];
-                if (x.message.content != null){
+                if (x.content != null){
                     // If we find an earlier submission from this user...
-                    if (x.message.content.includes(interaction.user.id) && x.message.content.includes("map submission [")){
+                    if (x.content.includes(interaction.user.id) && x.content.includes("map submission [")){
                         // If we're preventing resubmission, check all messages for a matching ID, cancel submission if match found
                         if (config.mtcSettings.preventResubmit){
-                            if (x.message.content.substring(x.message.content.indexOf(`\``)+1, x.message.content.lastIndexOf(`\``)) == mapId){
+                            if (x.content.substring(x.content.indexOf(`\``)+1, x.content.lastIndexOf(`\``)) == mapId){
                                 isValid = false;
-                                interaction.editReply({content:`This map has already been submitted, most recently on ${x.message.createdTimestamp.toLocaleString()}.`, flags:MessageFlags.Ephemeral})
+                                interaction.editReply({content:`This map has already been submitted, most recently on ${x.createdTimestamp.toLocaleString()}.`, flags:MessageFlags.Ephemeral})
                                 return isValid;
                             }
                         }
                         // If we're slowing submissions, block submission if it's been less than the cooldown
                         else if (config.mtcSettings.submitCooldownHours > 0){
-                            if (((currentDate - x.message.createdTimestamp)/3600000).toFixed(2) < config.mtcSettings.submitCooldownHours){
+                            if (((currentDate - x.createdTimestamp)/3600000).toFixed(2) < config.mtcSettings.submitCooldownHours){
                                 isValid = false;
-                                interaction.editReply({content:`You are submitting too frequently. Try again in ${((config.mtcSettings.submitCooldownHours*60) - ((currentDate - x.message.createdTimestamp)/60000)).toFixed(2)} minutes.`,flags:MessageFlags.Ephemeral})
+                                interaction.editReply({content:`You are submitting too frequently. Try again in ${((config.mtcSettings.submitCooldownHours*60) - ((currentDate - x.createdTimestamp)/60000)).toFixed(2)} minutes.`,flags:MessageFlags.Ephemeral})
                                 return isValid;
                             }
                         }
